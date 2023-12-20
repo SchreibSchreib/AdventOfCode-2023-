9c0a4eac49ea37fe5cc9f9671bea90f71a2efc68
“SchreibSchreib” <felixschreiber89@gmail.com>
2023-12-20 11:40:17 +0100

    Finished Day19 Part 1

--- Day19/PredicateEvaluator.cs
+++ Day19/PredicateEvaluator.cs
@@ -11,20 +11,84 @@ namespace Day19
     {
         public int Result { get; }
 
-        private string _targetAfterExpression;
-        private bool _resultExpression;
-        private KeyValuePair<string, string[]> _allPredicatesForEntry;
-        private Func<int,int,bool> _predicate;
+        private string _entryPoint;
+        private Dictionary<string, int> _parts;
+        private Dictionary<string, string[]> _allPredicatesForEntry;
+        private Func<int, int, bool> _greaterThan = (x, y) => x > y;
+        private Func<int, int, bool> _lessThan = (x, y) => x < y;
 
-        public PredicateEvaluator(string currentEntry, Dictionary<string, string[]> allWorkflows)
+        public PredicateEvaluator(string entryPoint,
+            Dictionary<string, int> parts,
+            Dictionary<string, string[]> allWorkflows)
         {
+            _entryPoint = entryPoint;
+            _parts = parts;
             _allPredicatesForEntry = allWorkflows;
-            _predicate = ConvertStringToPredicate(_allPredicatesForEntry);
+            Result = NavigateThroughWorkflowsAndEvaluate();
         }
 
-        private Func<int, int, bool> ConvertStringToPredicate(string predicateString)
+        private int NavigateThroughWorkflowsAndEvaluate()
         {
-            throw new NotImplementedException();
+            if(IsAccepted())
+            {
+                return SumPartsUp();
+            }
+            return 0;
         }
+
+        private int SumPartsUp() => _parts.Values.Sum();
+
+        private bool IsAccepted() => GetLastEntry() == "A";
+
+        private string GetLastEntry()
+        {
+            while (_entryPoint != "A" && _entryPoint != "R")
+            {
+                _entryPoint = GetNextEntryPoint();
+            }
+
+            return _entryPoint;
+        }
+
+        private string GetNextEntryPoint()
+        {
+            string result = _entryPoint;
+            foreach (var entry in _allPredicatesForEntry[_entryPoint])
+            {
+                if (IsValidEvaluation(entry))
+                {
+                    var afterEvaluation = Evaluate(entry);
+                    if (_entryPoint != afterEvaluation)
+                    {
+                        return afterEvaluation;
+                    }  
+                }
+                else
+                {
+                    result = entry;
+                }
+            }
+            return result;
+        }
+
+        private string Evaluate(string predicate)
+        {
+            int partsNumber = _parts[Convert.ToString(predicate[0])];
+            int predicateNumber = int.Parse(predicate.Substring(2).Split(":")[0]);
+            string newResult = predicate.Split(":")[1];
+
+            if (predicate.Contains(">"))
+            {
+                if(_greaterThan.Invoke(partsNumber, predicateNumber)) return newResult;
+            }
+            if (predicate.Contains("<"))
+            {
+                if (_lessThan.Invoke(partsNumber, predicateNumber)) return newResult;
+            }
+
+            return _entryPoint;
+        }
+
+        private bool IsValidEvaluation(string entry) => entry.Contains(":");
     }
 }
--- Day19/Program.cs
+++ Day19/Program.cs
@@ -2,13 +2,20 @@
 
 var input = new Content().Get;
 
-var test = new WorkflowDictionaryCreator(input[0]).WorkFlowToPredicates;
+var allWorkflowsWithPredicates = new WorkflowDictionaryCreator(input[0]).WorkFlowToPredicates;
 var RankingOfParts = new AcceptedParts(input[1]).Get;
 string keyToFind = "in";
+int result = 0;
 
 foreach (var part in RankingOfParts)
 {
-    
+    result += new PredicateEvaluator(keyToFind, part, allWorkflowsWithPredicates).Result;
+    Console.WriteLine(result);
 }
 
+Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Total result: {result}");
+
+Console.ReadLine();
